f8436fb551ad1036cee3b2499a67435bfad7060d
z98334818-blip <z98334818@gmail.com>
2026-06-09 22:12:32 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8606776997:AAElzO6hH5KGT7dQYa-XM8egnNfc3wRrAY8")
 ADMIN_IDS = [int(id) for id in os.getenv("ADMIN_IDS", "6486215227").split(",")]
-DB_PATH = "bot.db"
+DB_PATH = os.path.join(os.path.dirname(__file__), "bot.db")
